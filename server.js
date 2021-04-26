e9d61fc43f4fe9a3ec3ec7874de8e5f2db26fefd
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-04-26 14:35:16 +0530

    MongoDB connection configured

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const path = require("path");
-
+const db = require("./config/mongodb-connnection");
 const app = express();
 const port = process.env.PORT || 7000; // Heroku will need the PORT environment variable
+
+// MongoDB connection
+db.connect((err)=>{
+  if (err) {
+    console.log("Database connection Error :"+err);
+  }else{
+    console.log("Database connected to port 27017");
+  }
+})
+
+
 
 app.use(express.static(path.join(__dirname, "build")));
 
